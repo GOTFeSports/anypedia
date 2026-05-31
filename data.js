70c0ee7a3509202b0fc23cb0cbbe6f40017d4811
Work <gotfdota@gmail.com>
2026-05-31 12:36:07 +0300

    Update data.js

--- data.js
+++ data.js
@@ -771,8 +771,8 @@ const tournaments = [
 },
     teamsList: [
       { name: "Podosinovik", logo: "podosinovik.png", roster: [ { nick: ":3", pos: 1 }, { nick: "зато уютно умирать", pos: 2 }, { nick: "МИШКА МОРГЕН", pos: 3 }, { nick: "El Oreshniko del Perú", pos: 4 }, { nick: "аурная зараза", pos: 5 } ] },
-      { name: "LETO Junior", logo: "dota2.png", roster: [ { nick: "", pos: 1 }, { nick: "", pos: 2 }, { nick: ", pos: 3 }, { nick: "", pos: 4 }, { nick: "", pos: 5 } ] },
-      { name: "Gitlz", logo: "gitlz.png", roster: [ { nick: "", pos: 1 }, { nick: "", pos: 2 }, { nick: ", pos: 3 }, { nick: "", pos: 4 }, { nick: "", pos: 5 } ] },
+      { name: "LETO Junior", logo: "dota2.png", roster: [ { nick: "", pos: 1 }, { nick: "", pos: 2 }, { nick: "", pos: 3 }, { nick: "", pos: 4 }, { nick: "", pos: 5 } ] },
+      { name: "Gitlz", logo: "gitlz.png", roster: [ { nick: "", pos: 1 }, { nick: "", pos: 2 }, { nick: "", pos: 3 }, { nick: "", pos: 4 }, { nick: "", pos: 5 } ] },
     ],
     
     casters: [
