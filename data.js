46cd649b5529eec0b7969967c468c2787ceb2727
Work <gotfdota@gmail.com>
2026-06-18 15:22:11 +0300

    Update data.js

--- data.js
+++ data.js
@@ -188,9 +188,9 @@ const tournaments = [
     "format": "Single Elimination",
     "organizer": "AnyLvL Community x GOTF eSports",
     "links": {
-        "dotabuff": "",
+        "dotabuff": "https://ru.dotabuff.com/esports/leagues/19830-anylvl-x-gotf-esports-special-3",
         "discord": "https://discord.gg/3N4Jg6Jpak",
-        "rules": "",
+        "rules": "https://docs.google.com/document/d/1EtWhOh2EJtNIVNVd-nOucqHPZ7aX2tB-/edit?usp=sharing&ouid=111035949132865620643&rtpof=true&sd=true",
         "bracket": ""
     },
     "teamsList": [],
@@ -198,9 +198,9 @@ const tournaments = [
         { "name": "TBD", "link": "" }
     ],
     "bracketEmbed": "",
-    "registrationLink": "",
-    "telegramLink": "https://t.me/gotf_dota",
-    "description": "Без описания.",
+    "registrationLink": "https://t.me/anylvlcommunity",
+    "telegramLink": "https://t.me/anylvlcommunity",
+    "description": "Квалификации на третий турнир серии Special от AnyLvL Community и GOTF eSports. На турнире как обычно используется формат Elimination Draft (Герои, выбранные в одной игре серии, не могут быть повторно выбраны в этой же серии). Вся открытая квалификация пройдет в формате Best of 1, а матчи за слот в формате Best of 3, принять участие может каждый. Победители Split Bracket (две сетки) попадут в основную часть турнира.",
     "prizePool": [
         { "place": "1-2", "amount": "Слот в CQ", "team": "" },
         { "place": "1-2", "amount": "Слот в CQ", "team": "" }
@@ -223,9 +223,9 @@ const tournaments = [
     "format": "Split Bracket",
     "organizer": "AnyLvL Community x GOTF eSports",
     "links": {
-        "dotabuff": "",
+        "dotabuff": "https://ru.dotabuff.com/esports/leagues/19830-anylvl-x-gotf-esports-special-3",
         "discord": "https://discord.gg/3N4Jg6Jpak",
-        "rules": "",
+        "rules": "https://docs.google.com/document/d/1EtWhOh2EJtNIVNVd-nOucqHPZ7aX2tB-/edit?usp=sharing&ouid=111035949132865620643&rtpof=true&sd=true",
         "bracket": ""
     },
     "teamsList": [
@@ -242,9 +242,9 @@ const tournaments = [
         { "name": "TBD", "link": "" }
     ],
     "bracketEmbed": "",
-    "registrationLink": "",
-    "telegramLink": "https://t.me/gotf_dota",
-    "description": "Без описания.",
+    "registrationLink": "https://t.me/anylvlcommunity",
+    "telegramLink": "https://t.me/anylvlcommunity",
+    "description": "Квалификации на третий турнир серии Special от AnyLvL Community и GOTF eSports. На турнире как обычно используется формат Elimination Draft (Герои, выбранные в одной игре серии, не могут быть повторно выбраны в этой же серии). Вся закрытая квалификация пройдет в формате Best of 3, примут участие 6 приглашенных команд и 2 попадут через открытые квалификации. Победители Split Bracket (две сетки) попадут в основную часть турнира.",
     "prizePool": [
         { "place": "1-2", "amount": "Слот в Special", "team": "" },
         { "place": "1-2", "amount": "Слот в Special", "team": "" },
@@ -267,7 +267,7 @@ const tournaments = [
     "links": {
         "dotabuff": "https://ru.dotabuff.com/esports/leagues/19830-anylvl-x-gotf-esports-special-3",
         "discord": "https://discord.gg/3N4Jg6Jpak",
-        "rules": "",
+        "rules": "https://docs.google.com/document/d/1EtWhOh2EJtNIVNVd-nOucqHPZ7aX2tB-/edit?usp=sharing&ouid=111035949132865620643&rtpof=true&sd=true",
         "bracket": ""
     },
     "teamsList": [
@@ -284,9 +284,9 @@ const tournaments = [
         { "name": "TBD", "link": "" }
     ],
     "bracketEmbed": "",
-    "registrationLink": "",
-    "telegramLink": "https://t.me/gotf_dota",
-    "description": "Без описания.",
+    "registrationLink": "https://t.me/anylvlcommunity",
+    "telegramLink": "https://t.me/anylvlcommunity",
+    "description": "Третий турнир серии Special от AnyLvL Community и GOTF eSports. На турнире как обычно используется формат Elimination Draft (Герои, выбранные в одной игре серии, не могут быть повторно выбраны в этой же серии). Весь турнир пройдет в формате Best of 3, примут участие 6 приглашенных команд и 2 попадут через закрытые квалификации. На грандфинале турнира в формате Best of 5 будет присутстовать звёзда мировой сцены.",
     "prizePool": [
         { "place": 1, "amount": "100%", "team": "" },
     ],
